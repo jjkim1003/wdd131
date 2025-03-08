223c35caa223f2003e4b236e1f48ca272366e2ba
jjkim1003 <jaejinkim@gmail.com>
2025-03-08 14:33:48 -0600

    updated

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Dynamically get the current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Dynamically get the last modified date of the document
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
