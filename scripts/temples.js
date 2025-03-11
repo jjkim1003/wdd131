8c5f2d3d471e42e0a1aee6e3f3cbd7fb62e8517e
jjkim1003 <jaejinkim@gmail.com>
2025-03-11 10:45:28 -0500

    updated

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,3 +3,13 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 // Dynamically get the last modified date of the document
 document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
+
+// Hamburger menu
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+    hamButton.innerHTML = hamButton.classList.contains('open') ? 'X' : '&#9776;';
+});
